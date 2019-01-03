09d1cb2eb7a925cd11c0baf401003a8361ac53d2
Sihan-Son <sihan830@gmail.com>
2019-01-03 23:15:54 +0900

    [UPDATE] Parsing with GUI

--- midi_parser/Form/MiDi.Designer.cs
+++ midi_parser/Form/MiDi.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace midi_parser
 {
-    partial class Form1
+    partial class Midi
     {
         /// <summary>
         /// Required designer variable.
@@ -28,11 +28,100 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.midiText = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.filePath = new System.Windows.Forms.TextBox();
+            this.fnBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.File = new System.Windows.Forms.TextBox();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // midiText
+            // 
+            this.midiText.Location = new System.Drawing.Point(14, 82);
+            this.midiText.Multiline = true;
+            this.midiText.Name = "midiText";
+            this.midiText.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.midiText.Size = new System.Drawing.Size(259, 208);
+            this.midiText.TabIndex = 0;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // filePath
+            // 
+            this.filePath.Location = new System.Drawing.Point(13, 13);
+            this.filePath.Name = "filePath";
+            this.filePath.Size = new System.Drawing.Size(178, 21);
+            this.filePath.TabIndex = 2;
+            // 
+            // fnBtn
+            // 
+            this.fnBtn.Location = new System.Drawing.Point(198, 13);
+            this.fnBtn.Name = "fnBtn";
+            this.fnBtn.Size = new System.Drawing.Size(75, 23);
+            this.fnBtn.TabIndex = 3;
+            this.fnBtn.Text = "open";
+            this.fnBtn.UseVisualStyleBackColor = true;
+            this.fnBtn.Click += new System.EventHandler(this.fnBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "File Name";
+            // 
+            // File
+            // 
+            this.File.Location = new System.Drawing.Point(80, 45);
+            this.File.Name = "File";
+            this.File.ReadOnly = true;
+            this.File.Size = new System.Drawing.Size(111, 21);
+            this.File.TabIndex = 5;
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(198, 45);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(75, 23);
+            this.btnRun.TabIndex = 6;
+            this.btnRun.Text = "Run";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // Midi
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(284, 301);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.File);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.fnBtn);
+            this.Controls.Add(this.filePath);
+            this.Controls.Add(this.midiText);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Midi";
+            this.Text = "MIDI";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox midiText;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox filePath;
+        private System.Windows.Forms.Button fnBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox File;
+        private System.Windows.Forms.Button btnRun;
     }
 }
--- midi_parser/Form/MiDi.cs
+++ midi_parser/Form/MiDi.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,11 +11,52 @@ using System.Windows.Forms;
 
 namespace midi_parser
 {
-    public partial class Form1 : Form
+    public partial class Midi : Form
     {
-        public Form1()
+        public Midi()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        
+        private void fnBtn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog opd = new OpenFileDialog();
+            opd.Filter = "Midi Files(*.mid) | *.mid";
+            opd.Title = "Select MIDI FILE";
+
+            if (opd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                this.filePath.Text = opd.FileName;
+            }
+            
+            string[] name = filePath.Text.Split('\\');
+            var pathLength = name.Length;
+            File.Text = name[pathLength - 1];
+
+        }
+
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            FileStream fs = new FileStream(this.filePath.Text, FileMode.Open);
+
+            var text = "";
+            while (fs.Position < fs.Length)
+            {
+                Chunk chunk = Chunk.Parse(fs);
+            
+                if (chunk != null)
+                {
+                    text += string.Format("{0} :{1} bytes \r\n", chunk.CTString, chunk.Length);
+                }
+            }
+            Console.Write(text);
+            midiText.AppendText(text);
         }
     }
 }
