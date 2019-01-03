d157b1afd5aeb42c7fc339ebebe9acf6ebd51f0b
Sihan-Son <sihan830@gmail.com>
2019-01-03 22:48:56 +0900

    [UPDATE] Show window form

--- midi_parser/Parser/Program.cs
+++ midi_parser/Parser/Program.cs
@@ -1,26 +1,39 @@
 using System;
 using System.IO;
+using System.Windows.Forms;
+
+
 
 
 namespace midi_parser
 {
     class Program
     {
-        static string fname = "../../midi/example.mid";
-
-        static void Main(string[] args)
+        /*
+         *  테스팅 내용
+         */
+        //        static string fname = "../../midi/example.mid";
+        //
+        //        static void Main(string[] args)
+        //        {
+        //            FileStream fs = new FileStream(fname, FileMode.Open);
+        //
+        //            while (fs.Position < fs.Length)
+        //            {
+        //                Chunk chunk = Chunk.Parse(fs);
+        //
+        //                if (chunk != null)
+        //                {
+        //                    Console.WriteLine("{0} :{1} bytes", chunk.CTString, chunk.Length);
+        //                }
+        //            }
+    
+        [STAThread]
+        static void Main()
         {
-            FileStream fs = new FileStream(fname, FileMode.Open);
-
-            while (fs.Position < fs.Length)
-            {
-                Chunk chunk = Chunk.Parse(fs);
-
-                if (chunk != null)
-                {
-                    Console.WriteLine("{0} :{1} bytes", chunk.CTString, chunk.Length);
-                }
-            }
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Midi());
         }
     }
 }
