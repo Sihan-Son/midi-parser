b79d3c2774caaa49b5a2108b25afb789d71c2431
Sihan-Son <sihan830@gmail.com>
2019-01-05 23:23:32 +0900

    [ADD] parse MIDI header

---
+++ midi_parser/Parser/Header.cs
@@ -0,0 +1,24 @@
+namespace midi_parser
+{
+    public class Header:Chunk
+    {
+        public int Format
+        {
+            get { return StaticFunc.ConvertHostorderS(Data, 0); }
+        }
+
+        public int TrackCount
+        {
+            get { return StaticFunc.ConvertHostorderS(Data, 2); }
+        }
+
+        public int Division
+        {
+            get { return StaticFunc.ConvertHostorderS(Data, 4); }
+        }
+        
+        public Header(int ctype, int length, byte[] buffer) : base(ctype, length, buffer)
+        {
+        }
+    }
+}
